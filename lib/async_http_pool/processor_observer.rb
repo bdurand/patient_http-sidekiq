b7124f104cdb2cf28dba9cd615ad4ae3ae1946c3
Brian Durand <bbdurand@gmail.com>
2026-02-04 08:01:43 -0800

    add yard comments

--- lib/async_http_pool/processor_observer.rb
+++ lib/async_http_pool/processor_observer.rb
@@ -6,21 +6,42 @@ module AsyncHttpPool
   # processor thread and so should be lightweight and not do processing other than recording
   # metrics or similar.
   class ProcessorObserver
+    # Called when the processor starts.
+    #
+    # @return [void]
     def start
     end
 
+    # Called when the processor stops.
+    #
+    # @return [void]
     def stop
     end
 
+    # Called when a request cannot be enqueued because the processor is at capacity.
+    #
+    # @return [void]
     def capacity_exceeded
     end
 
+    # Called when a request starts processing.
+    #
+    # @param request_task [RequestTask] the request task that started
+    # @return [void]
     def request_start(request_task)
     end
 
+    # Called when a request finishes processing.
+    #
+    # @param request_task [RequestTask] the request task that ended
+    # @return [void]
     def request_end(request_task)
     end
 
+    # Called when a request encounters an error.
+    #
+    # @param error [Error] the error that occurred
+    # @return [void]
     def request_error(error)
     end
   end
